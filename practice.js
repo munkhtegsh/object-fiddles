5231c0a336912b52c5aad7810343ca4d100a0d2a
Munkhtegsh Munkhbat <munkhtegsh@munkhtegshsimac.attlocal.net>
2017-12-01 08:03:36 -0800

    It will pass the all the problems in the project.

--- practice.js
+++ practice.js
@@ -150,8 +150,8 @@ user2.email = 'tyler.mcginnis@devmounta.in';
 
 //Create an empty object called methodCollection.
 
-  //Code Here
-
+//Code Here
+var methodCollection = {};
 
 
 
@@ -161,8 +161,9 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
  which logs 'hello' to the console. 
  */
 
-  //Code Here
-
+//Code Here
+methodCollection.alertHello = function () { alert('hello') };
+methodCollection.logHello = function () { console.log('hello') };
 
 
 
@@ -170,23 +171,8 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 //Now call your alertHello and logHello methods.
 
   //Code Here
-
-
-
-
-
-
-
-
-//NEXT PROBLEM
-
-
-
-// Create a function called makePerson which takes in name, birthday, ssn as its
-// parameters and returns a new object with all of the information that you passed in.
-
-  //Code Here
-
+// methodCollection.alertHello();
+// methodCollection.logHello();
 
 
 
@@ -198,8 +184,42 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
 
 
-// Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object and returns that object so that whenever you invoke makeCard, you get a brand new credit card.
+// Create a function called makePerson which takes in name, birthday, 
+//ssn as its
+// parameters and returns a new object with all of the information
+// that you passed in.
 
   //Code Here
+var makePerson = (name, birthday, ssn) => { 
+  var newObj = {};
+  newObj.name = name;
+  newObj.birthday = birthday;
+  newObj.ssn = ssn;
+  return newObj;
+ }
 
+
+
+
+
+
+
+//NEXT PROBLEM
+
+
+
+// Create a function called makeCard which takes in cardNumber, 
+//expirationDate, and securityCode to make a Credit Card object 
+//and returns that object so that whenever you invoke makeCard, 
+//you get a brand new credit card.
+
+  //Code Here
+var makeCard = (cardNumber, expirationDate, securityCode) => {
+  var creditCard = {
+    cardNumber : cardNumber,
+    expirationDate : expirationDate,
+    securityCode : securityCode
+  }
+  return creditCard;
+}
 
